3dd69a54273e5e3124e0efb0a4ee557cc7f0d6f7
Gab0410 <gabrielmcorlando@gmail.com>
2025-04-25 22:56:14 -0300

    Adding: T2-Finalizado

---
+++ T2/analisador-sintatico-t2/src/test/java/br/ufscar/dc/compiladores/analisador/sintatico/t2/AppTest.java
@@ -0,0 +1,20 @@
+package br.ufscar.dc.compiladores.analisador.sintatico.t2;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
